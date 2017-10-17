cf14dcda678a7ab34d8b85562532b9e3180633b6
Kuroko <fairyland0902@gmail.com>
2017-10-18 00:57:33 +0800

    Follow the experiment instruction to implement the system boot / startup

---
+++ arch/mips32/start.s
@@ -0,0 +1,17 @@
+.globl exception
+.globl start
+.extern init_kernel
+
+.set noreorder
+.set noat
+.align 2
+
+exception:
+
+.org 0x1000
+start:
+    lui     $sp, 0x8100
+    la		$gp, _gp
+    j		init_kernel
+    nop
+    
