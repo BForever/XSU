73cb1875bcae1de8d10ac72fc77a9e5c39aac8a4
Kuroko <fairyland0902@gmail.com>
2017-10-18 23:16:20 +0800

    Finish exp1, waiting for test on SWORD

---
+++ kernel/init.c
@@ -0,0 +1,28 @@
+#include <arch.h>
+#include <init_place_holder.h>
+
+void init_kernel()
+{
+    // Exception
+    init_exception();
+    // System call
+    init_syscall();
+    // Page table
+    init_pgtable();
+    // Drivers
+    init_vga();
+    init_ps2();
+    // Memory management
+    init_mem();
+    // file system
+    init_fs();
+    // Process control
+    init_pc();
+    // Interrupts
+    init_interrupts();
+    // Init finished, write seg
+    *GPIO_SEG = 0x11223344;
+    // Halt
+    while (1)
+        ;
+}
